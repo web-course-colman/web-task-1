e3c8facf3b9d69c60b1cacbd9d1fb69c45369d3c
guyYablonka <guyyablonka@gmail.com>
2025-12-30 20:23:13 +0200

    creat post controller

---
+++ src/controllers/post.js
@@ -0,0 +1,27 @@
+const Post = require("../models/Post");
+
+const addPost = async (req, res) => {
+  try {
+    const { message, sender } = req.body;
+
+    if (!message || !sender) {
+      return res
+        .status(400)
+        .json({ message: "Message and sender are required" });
+    }
+
+    const post = new Post({
+      message,
+      sender,
+    });
+
+    const savedPost = await post.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  addPost,
+};
